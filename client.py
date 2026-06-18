07a4d173c895c309ed31e86533d836363dbb9f07
Charlotte W <152224795+108charlotte@users.noreply.github.com>
2026-06-18 09:09:28 -0400

    (,)

--- client.py
+++ client.py
@@ -4,9 +4,9 @@ import time
 import json
 
 with xmlrpc.client.ServerProxy("http://141/165/50/132:6363") as proxy: 
-    print(proxy.system.listMethods())
-    help_message = proxy.system.methodHelp("heartbeat")
-    print(help_message)
+    # print(proxy.system.listMethods())
+    # help_message = proxy.system.methodHelp("heartbeat")
+    # print(help_message)
     # call method
     # proxy.system.methodSignature("heartbeat")
     proxy.heartbeat(json.dumps({"student_name": "charlotte", "timestamp": time.time()}))
