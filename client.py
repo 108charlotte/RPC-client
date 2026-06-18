40f8ff670c64f6bb6603d06932dfc22ead85512f
Charlotte W <152224795+108charlotte@users.noreply.github.com>
2026-06-18 09:27:26 -0400

    all in one file

--- client.py
+++ client.py
@@ -4,10 +4,5 @@ import time
 import json
 
 with xmlrpc.client.ServerProxy("http://141.165.50.132:6363") as proxy: 
-    # print(proxy.system.listMethods())
-    # help_message = proxy.system.methodHelp("heartbeat")
-    # print(help_message)
-    # call method
-    # proxy.system.methodSignature("heartbeat")
     proxy.heartbeat(json.dumps({"student_name": "charlotte", "timestamp": int(time.time())}))
 # server.system.listMethods()
---
+++ combined.py
@@ -0,0 +1,39 @@
+# https://docs.python.org/3/library/xmlrpc.html
+import xmlrpc.client
+import xmlrpc.server
+import time
+import json
+from xmlrpc.server import SimpleXMLRPCServer, SimpleXMLRPCRequestHandler
+import threading
+
+# client
+def run_client(): 
+    with xmlrpc.client.ServerProxy("http://141.165.50.132:6363") as proxy: 
+        while True: 
+            proxy.heartbeat(json.dumps({"student_name": "charlotte", "timestamp": int(time.time())}))
+            time.sleep(10) # 10 second sleep before sending another heartbeat ping
+
+# server
+def run_server(): 
+    with SimpleXMLRPCServer(('localhost', 8000)) as server: 
+        server.register_introspection_functions()
+
+        def heartbeat(json_string): 
+            context = json.load(json_string)
+            name = context["student_name"]
+            timestamp = context["timestamp"]
+            if time.time() - timestamp < 43200: # 12 hours equivalent in epoch seconds
+                return xmlrpc.client
+
+        server.register_function(heartbeat, "heartbeat")
+        server.serve_forever()
+
+# https://www.geeksforgeeks.org/python/multithreading-python-set-1/
+client_thread = threading.Thread(target=run_client)
+server_thread = threading.Thread(target=run_server)
+
+client_thread.start()
+server_thread.start()
+
+client_thread.join()
+server_thread.join()
