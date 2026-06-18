657f4e14b54e2af1030ea9eb1101599fee2f2308
Charlotte W <152224795+108charlotte@users.noreply.github.com>
2026-06-18 09:19:25 -0400

    updated server

--- server.py
+++ server.py
@@ -1,13 +1,18 @@
 import xmlrpc.server
 import time
 import xmlrpc.client
+import json
+from xmlrpc.server import SimpleXMLRPCServer, SimpleXMLRPCRequestHandler
 
-# THIS DOES NOT WORK AND I'M TRYING TO FIGURE THIS OUT
+with SimpleXMLRPCServer(('localhost', 8000)) as server: 
+    server.register_introspection_functions()
 
-with xmlrpc.server.SimpleXMLRPCServer(('localhost', 8000)) as server: 
-    def heartbeat(): 
-        name = xmlrpc.client.context["student_name"]
+    def heartbeat(json_string): 
+        context = json.load(json_string)
+        name = context["student_name"]
         timestamp = context["timestamp"]
         if time.time() - timestamp < 43200: # 12 hours equivalent in epoch seconds
             return xmlrpc.client
+
     server.register_function(heartbeat, "heartbeat")
+    server.serve_forever()
