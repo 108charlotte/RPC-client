e046ebc06da51733036e584d7edb908f0ca3fb7c
Charlotte W <152224795+108charlotte@users.noreply.github.com>
2026-06-18 09:04:41 -0400

    updated so sending info correctly hopefully

--- client.py
+++ client.py
@@ -1,12 +1,13 @@
 # https://docs.python.org/3/library/xmlrpc.html
 import xmlrpc.client
 import time
+import json
 
-with xmlrpc.client.ServerProxy("http://10.38.105.40", context={"student_name": "charlotte", "timestamp": time.time()}) as proxy: 
+with xmlrpc.client.ServerProxy("http://192.168.137.153:6363") as proxy: 
     print(proxy.system.listMethods())
     help_message = proxy.system.methodHelp("heartbeat")
     print(help_message)
     # call method
     # proxy.system.methodSignature("heartbeat")
-    proxy.heartbeat()
+    proxy.heartbeat(json.dumps({"student_name": "charlotte", "timestamp": time.time()}))
 # server.system.listMethods()
--- server.py
+++ server.py
@@ -2,9 +2,11 @@ import xmlrpc.server
 import time
 import xmlrpc.client
 
+# THIS DOES NOT WORK AND I'M TRYING TO FIGURE THIS OUT
+
 with xmlrpc.server.SimpleXMLRPCServer(('localhost', 8000)) as server: 
-    def heartbeat(context): 
-        name = context["student_name"]
+    def heartbeat(): 
+        name = xmlrpc.client.context["student_name"]
         timestamp = context["timestamp"]
         if time.time() - timestamp < 43200: # 12 hours equivalent in epoch seconds
             return xmlrpc.client
