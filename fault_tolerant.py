e0b3aae262f8d4ea4c10827eb35e683372d6237c
Charlotte M Woodrum <cw45305@georgiasouthern.edu>
2026-06-19 11:51:42 -0400

    threading

--- fault_tolerant.py
+++ fault_tolerant.py
@@ -4,7 +4,10 @@ import time
 import json
 from xmlrpc.server import SimpleXMLRPCServer, SimpleXMLRPCRequestHandler
 import threading
-import difflib.get_close_matches
+import difflib
+import socket
+
+socket.setdefaulttimeout(5)
 
 server_name = "Charlotte"
 server_ip_addr = "141.165.50.133"
@@ -18,16 +21,17 @@ def send_heartbeat_to_ip(ip_dest, student_name, timestamp, ip_address, string_id
     try: 
         with xmlrpc.client.ServerProxy("http://" + ip_dest + ":6363") as proxy: 
             print()
-            print(f"{string_identifier}: Sending heartbeat to {ip_dest}")
+            if string_identifier != "CLIENT": 
+                print(f"{string_identifier}: Sending heartbeat with name: {student_name}, timestamp: {timestamp}, and ip address: {ip_address} to {ip_dest}")
             error = proxy.heartbeat(json.dumps({"student_name": student_name, "timestamp": timestamp, "ip_address": ip_address}))
             print(f"{string_identifier}: Sent heartbeat to {ip_dest}, server returned code {error}")
-            return "Success"
     except Exception as e: 
-        return e
+        print(f"{string_identifier}: Error - {e}")
 
 def get_alphabetical_next_index_from_me(): 
+    global client_list
     client_names = [client["student_name"] for client in client_list]
-    after_me_alphabetically = [name for name in client_names if name > server_name]s
+    after_me_alphabetically = [name for name in client_names if name > server_name]
     closest = difflib.get_close_matches(server_name, after_me_alphabetically, 1)
     client_index = 0
     for i, client in enumerate(client_list): 
@@ -37,39 +41,31 @@ def get_alphabetical_next_index_from_me():
     return client_index
 
 def get_next_client_index(curr_index): 
+    global client_list
     return (curr_index + 1) % len(client_list)
 
 # client
 def run_client(): 
-    # choose who to ping based on who is live
-    if len(client_list) > 1: 
-        # assumes client list currently in order; every time a new client is added, it is put back in order
-        my_position = next((index for (index, d) in enumerate(client_list) if d["student_name"] == "Charlotte"), None)
-        if my_position is not None: 
-            server_to_send_to = client_list[(my_position + 1) % len(client_list)]
-            ip_addr_to_send = server_to_send_to["ip_address"]
-    else: 
-        if first_few_heartbeats: 
-            ip_addr_to_send = start_server_to_ping
-        else: 
-            print("CLIENT: No one to send heartbeat to :(")
-            send_heartbeat = False
-    send_heartbeat = True
+    global num_heartbeats_sent
+    global client_list
     while True: 
         print(f"\nRUNNING CLIENT")
-        if num_heartbeats_sent < 3: 
+        if num_heartbeats_sent < 2: 
             ip_addr_to_send_to = start_server_to_ping
             send_heartbeat = True
         else: 
             if len(client_list) > 1: 
-                ip_addr_to_send_to = get_alphabetical_next_index_from_me()
+                ip_addr_to_send_to = client_list[get_alphabetical_next_index_from_me()]["ip_address"]
                 send_heartbeat = True
             else: 
                 print(f"CLIENT: No one available to heartbeat to")
                 send_heartbeat = False
         
         if send_heartbeat: 
-            send_heartbeat_to_ip(ip_addr_to_send_to, "Charlotte", time.time(), server_ip_addr, "CLIENT")
+            print(f"CLIENT: Sending my own heartbeat to {ip_addr_to_send_to}")
+            threading.Thread(target=send_heartbeat_to_ip, args=(ip_addr_to_send_to, "Charlotte", time.time(), server_ip_addr, "CLIENT"), daemon=True).start()
+            # send_heartbeat_to_ip(ip_addr_to_send_to, "Charlotte", time.time(), server_ip_addr, "CLIENT")
+            num_heartbeats_sent += 1
 
         time.sleep(10) # 10 second sleep before sending another heartbeat ping/checking if anyone available to send heartbeat ping to
 
@@ -87,7 +83,7 @@ def heartbeat(json_string):
         print(f"\nSERVER: Someone sent Charlotte to herself, don't propagate")
         return 0
     
-    if abs(time.time() - timestamp) < 60:
+    if abs(time.time() - timestamp) < 20:
         print(f"SERVER: Timestamp {timestamp} valid for {name} at {ip_addr}")
         all_ip_addrs = [client["ip_address"] for client in client_list]
         # need to include timestamps and update
@@ -103,10 +99,12 @@ def heartbeat(json_string):
             client_index = next((index for (index, d) in enumerate(client_list) if d["ip_address"] == ip_addr), None)
             if client_index is None: 
                 print(f"SERVER: BUG - client index not found in list")
+                return 1
             else: 
                 client_list[client_index]["timestamp"] = timestamp
     else: 
         print(f"SERVER: Received invalid timestamp from {name} at {ip_addr} - {timestamp} was {abs(timestamp - time.time())} off")
+        return 1
     
     # go through client list and remove anything with a timestamp over 1 min old EXCEPT for my own client
     client_list = [client for client in client_list if abs(client["timestamp"] - time.time()) < 60]
@@ -122,10 +120,9 @@ def heartbeat(json_string):
         print(f"SERVER: Skipping {old_item['student_name']} to go to {next_item['student_name']}")
     
     print(f"SERVER: forwarding {name} to {next_item['student_name']} (sending to IP: {next_item['ip_address']})")
-    response = send_heartbeat_to_ip(next_item["ip_address"], name, timestamp, ip_addr, "SERVER")
-    if response == "Success": 
-        return 0
-    return 1
+    threading.Thread(target=send_heartbeat_to_ip, args=(next_item["ip_address"], name, timestamp, ip_addr, "SERVER"), daemon=True).start()
+    # response = send_heartbeat_to_ip(next_item["ip_address"], name, timestamp, ip_addr, "SERVER")
+    return 0
 
 def run_server(): 
     with SimpleXMLRPCServer(('141.165.50.133', 6363)) as server: 
